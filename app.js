d19d3a42acfdd930cb98fca614c5e0eda982bb4e
amailemon98 <amailemon98@gmail.com>
2024-06-19 20:39:32 +0900

    get root

--- app.js
+++ app.js
@@ -22,7 +22,18 @@ app.use((req, res, next)=>{
 })
 
 
+app.get('/', (req, res) => {
+    const str = `
+    <h1>root</h1>
+    GET: localhost:4500/makeup <br/>
+    GET: localhost:4500/board <br/>
+    POST: localhost:4500/board <br/>
+    PUT: localhost:4500/board/:id <br/>
+    DELETE: localhost:4500/board/:id <br/>
+    `
 
+    res.send(str)
+})
 app.use('/makeup', require('./routes/makeup'));
 app.use('/board', require('./routes/board'));
 
